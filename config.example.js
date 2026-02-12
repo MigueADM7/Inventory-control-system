612205e040e321c78d02261440d0887388f99686
MigueADM7 <migue.adm1105@gmail.com>
2026-02-12 13:05:48 -0500

    Configuration example for other developers

---
+++ config.example.js
@@ -0,0 +1,9 @@
+// config.example.js - Ejemplo de configuración para otros desarrolladores
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY_AQUÍ",
+  authDomain: "TU_AUTH_DOMAIN_AQUÍ",
+  projectId: "TU_PROJECT_ID_AQUÍ",
+  storageBucket: "TU_STORAGE_BUCKET_AQUÍ",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID_AQUÍ",
+  appId: "TU_APP_ID_AQUÍ"
+};
